e70cbb947bc971dc56c25ac5be690605118af629
Darbaz Ali <darbaz.me@gmail.com>
2022-02-15 06:53:44 +0300

    short hand variables

--- basics/main.go
+++
@@ -1,8 +0,0 @@
-package main
-
-import "fmt"
-
-func main() {
-	i := 42
-	fmt.Println(i)
-}
---
+++ variables.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	dogsName := "Max"
+	dogsAge := 1
+	dogsHight := 0.33
+
+	fmt.Printf("%v, %T \n", dogsName, dogsName)
+	fmt.Printf("%v, %T \n", dogsAge, dogsAge)
+	fmt.Printf("%v, %T \n", dogsHight, dogsHight)
+}
