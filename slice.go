375d8554fa73141f65826602c5980e16a1cb2a6d
Darbaz Ali <darbaz.me@gmail.com>
2022-02-18 21:50:28 +0300

    copy a slice into another

--- slice.go
+++ slice.go
@@ -7,4 +7,9 @@ func main() {
 	x := make([]int, 5, 10)
 	x = append(x, 1, 2, 3, 4, 5)
 	fmt.Println(x)
+
+	slice1 := []int{1, 2, 3}
+	slice2 := make([]int, 2)
+	copy(slice2, slice1)
+	fmt.Println(slice1, slice2)
 }
