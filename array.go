bd9bfb5f3233cf3563e4431c803fb1a3cb75d150
Darbaz Ali <darbaz.me@gmail.com>
2022-02-18 20:50:51 +0300

    array creating - short syntax

--- array.go
+++ array.go
@@ -3,6 +3,8 @@ package main
 import "fmt"
 
 func main() {
+
+	// array creation - first form
 	var x [5]int
 	x[0] = 1
 	x[1] = 2
@@ -11,4 +13,9 @@ func main() {
 	x[4] = 5
 
 	fmt.Println(x)
+
+	// array creating - short syntax
+	arr := [5]int{83, 92, 82, 32, 65}
+
+	fmt.Println(arr)
 }
